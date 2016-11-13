010c60c61b29c5f91e8b08d817b82a1494188d26
Artem Tyurin <artem.tyurin@gmail.com>
2016-11-13 01:08:30 +0100

    Make it pretty

--- week6-1.py
+++ week6-1.py
@@ -11,15 +11,16 @@ def printAndWriteAnswer(index, answer):
     f.write(str(answer))
     f.close()
 
-def getClusteredImages(X, cluster_labels):
-    X_median = np.zeros_like(X);
-    X_average = np.zeros_like(X);
+def getClusteredImages(original_image, cluster_labels):
+    X_median = np.zeros_like(original_image);
+    X_average = np.zeros_like(original_image);
 
     for k in range(0, n_clusters):
         kth_cluster = [i for i, v in enumerate(cluster_labels) if v == k]
+        kth_cluster_original = [original_image[i] for i in kth_cluster]
 
-        kth_cluster_median = np.median([X[i] for i in kth_cluster], axis=0)
-        kth_cluster_average = np.average([X[i] for i in kth_cluster], axis=0)
+        kth_cluster_median = np.median(kth_cluster_original, axis=0)
+        kth_cluster_average = np.average(kth_cluster_original, axis=0)
 
         for i in kth_cluster:
             X_median[i] = kth_cluster_median
@@ -27,12 +28,12 @@ def getClusteredImages(X, cluster_labels):
 
     return (X_median, X_average)
 
-def PSNR(I, K):
+def PSNR(I, K, size):
     MAX = 1 # because we are using img_as_float
 
     mse = np.sum(
-        [(r*r + g*g + b*b) for (r, g, b) in (I - K)]) / (M_width * M_height * 3
-    )
+        [(r*r + g*g + b*b) for (r, g, b) in (I - K)]
+    ) / (size * 3)
 
     return 20 * np.log10(MAX) - 10 * np.log10(mse)
 
@@ -41,10 +42,11 @@ image = imread('parrots.jpg')
 n_clusters = 11
 clf = KMeans(n_clusters=n_clusters, init='k-means++', random_state=241)
 
-M = img_as_float(image)
-M_width, M_height, _ = np.shape(M)
+I = img_as_float(image)
+I_shape = np.shape(I)
+I_size = I_shape[0] * I_shape[1]
 
-X = np.reshape(M, (-1, 3))
+X = np.reshape(I, (-1, 3))
 clf.fit(X)
 
 X_median, X_average = getClusteredImages(X, clf.labels_)
@@ -52,11 +54,11 @@ X_median, X_average = getClusteredImages(X, clf.labels_)
 print(
     'Cluster count: {}, PSNR for median: {}, average: {}'.format(
         n_clusters,
-        PSNR(X, X_median),
-        PSNR(X, X_average)
+        PSNR(X, X_median, I_size),
+        PSNR(X, X_average, I_size)
     )
 )
 
 printAndWriteAnswer(1, n_clusters)
 
-imsave('parrots_median.jpg', np.reshape(X_median, (M_width, M_height, 3)))
+imsave('parrots_median.jpg', np.reshape(X_median, I_shape))
